f3fa0a6e77bf11e20d634fab25781bdbd33744da
Andrey <33428293+Russia9@users.noreply.github.com>
2018-05-08 21:01:47 +0300

    Disabled logging
    Disabled standard jnativehook logging

--- src/main/java/org/russia9/Main.java
+++ src/main/java/org/russia9/Main.java
@@ -1,16 +1,26 @@
 package org.russia9;
 
+import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 
 import java.awt.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
-    static Manager manager;
+    private static Manager manager;
 
     public static void main(String[] args) throws NativeHookException, AWTException, InterruptedException {
         System.out.println("MagicHelper v0.1-SNAPSHOT by Russia9");
         System.out.println("For more information: https://github.com/Russia9/MagicHelper");
-        //System.err.println("MagicHelper v0.1 haven't hook and start is unable");
+
+        System.err.println("MagicHelper v0.1 doesn't have full functional");
+
+        // Disabling standard jnativehook logging
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.WARNING);
+        logger.setUseParentHandlers(false);
+
         manager = new Manager();
     }
 }
