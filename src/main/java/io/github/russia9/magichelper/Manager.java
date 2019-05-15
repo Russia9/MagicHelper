48e7925fce310c2fb2b6925a53f6dc1cf40690dc
Russia9 <a.kovalev9@hotmail.com>
2019-05-15 23:11:29 +0300

    Logging #1

--- src/main/java/io/github/russia9/magichelper/Manager.java
+++ src/main/java/io/github/russia9/magichelper/Manager.java
@@ -6,6 +6,7 @@ import io.github.russia9.magichelper.modules.autoclicker.Clicker;
 import io.github.russia9.magichelper.modules.autominer.Miner;
 import io.github.russia9.magichelper.modules.buttonclamper.ButtonClamper;
 import org.apache.commons.lang3.SystemUtils;
+import org.apache.log4j.Logger;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
@@ -18,6 +19,8 @@ import java.awt.event.MouseEvent;
  * Main class
  */
 public class Manager {
+    private static Logger logger = Logger.getLogger(Manager.class.getName());
+
     private Clicker clicker;
     private ButtonClamper clamper;
     private Miner miner;
@@ -40,15 +43,19 @@ public class Manager {
     private int mouseCode = -1;
 
     Manager() throws NativeHookException {
-        // Init clicker and miner
+        // Init modules
+        logger.trace("Initialising modules");
         clicker = new Clicker(this);
         clamper = new ButtonClamper();
         miner = new Miner();
 
         // Default keys
+        logger.trace("Setting default variables");
         if (SystemUtils.IS_OS_WINDOWS) { // WINDOWS
+            logger.trace("Detected OS: WIN");
             setAutoclickerActivateButton(Reference.AUTOCLICKER_DEFAULT_WIN_ACTIVATE_BUTTON);
         } else { // *NIX and MAC
+            logger.trace("Detected OS: NIX");
             setAutoclickerActivateButton(Reference.AUTOCLICKER_DEFAULT_NIX_ACTIVATE_BUTTON);
         }
         setAutoMinerActivateButton(Reference.AUTOMINER_DEFAULT_ACTIVATE_BUTTON);
@@ -57,6 +64,7 @@ public class Manager {
         setAutoclickerClickButton(Reference.AUTOCLICKER_DEFAULT_CLICK_BUTTON);
 
         // Registering events
+        logger.trace("Registering events");
         GlobalScreen.registerNativeHook();
         Hook hook = new Hook(this);
         GlobalScreen.addNativeMouseListener(hook);
@@ -67,6 +75,7 @@ public class Manager {
     public void mousePressed(NativeMouseEvent nativeMouseEvent) throws AWTException {
         if (nativeMouseEvent.getButton() == autoclickerActivateButton) {
             mainButton = true;
+            logger.trace("MainButton Pressed");
         } else {
             if (mainButton) {
                 mouseCode = MouseEvent.getMaskForButton(nativeMouseEvent.getButton());
@@ -82,24 +91,31 @@ public class Manager {
     public void mouseReleased(NativeMouseEvent nativeMouseEvent) throws AWTException {
         if (nativeMouseEvent.getButton() == autoclickerActivateButton) {
             if (clicker.isAlive()) {
+                logger.debug("Clicker stopped");
                 clicker.stop();
             } else if (miner.isAlive()) {
                 miner.stop();
+                logger.debug("Miner stopped");
             } else if (clamper.isAlive()) {
                 clamper.stop();
+                logger.debug("Clamper stopped");
             } else {
                 if (keyCode == -1 && mouseCode == -1) {
                     clicker.start();
+                    logger.debug("Clicker(type: 0; button: " + getAutoclickerClickButton() + "; time:" + getAutoclickerClickTime() + ") started");
                 } else if (mouseCode != -1) {
                     clicker.start(0, mouseCode, autoclickerClickTime);
+                    logger.debug("Clicker(type: 0; button: " + mouseCode + "; time:" + getAutoclickerClickTime() + ") started");
                     mouseCode = -1;
                 } else {
                     clicker.start(1, keyCode, autoclickerClickTime);
+                    logger.debug("Clicker(type: 1; button: " + keyCode + "; time:" + getAutoclickerClickTime() + ") started");
                     keyCode = -1;
                 }
             }
             mainButton = false;
             keyCode = -1;
+            logger.trace("MainButton released");
         }
     }
 
@@ -125,12 +141,15 @@ public class Manager {
             switch (minerKeyCode) {
                 case 87: // Horizontal mining
                     miner.start(0);
+                    logger.debug("Miner(type: 0) started");
                     break;
                 case NativeKeyEvent.VC_D: // Vertical mining
                     miner.start(1);
+                    logger.debug("Miner(type: 1) started");
                     break;
                 case NativeKeyEvent.VC_S: // Smart mining
                     miner.start(2);
+                    logger.debug("Miner(type: 2) started");
                     break;
                 default:
                     break;
@@ -140,6 +159,7 @@ public class Manager {
         } else if (Helper.getKeyCode(nativeKeyEvent) == buttonClamperActivateButton) {
             if (clamperCode != -1 && clamperType != -1) {
                 clamper.start(clamperType, clamperCode);
+                logger.debug("Clamper(type: " + clamperType + "; button: " + clamperCode + ") started");
             }
             clamperCode = -1;
             clamperType = -1;
