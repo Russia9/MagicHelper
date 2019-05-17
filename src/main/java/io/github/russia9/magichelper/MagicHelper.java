5f27fb7e5291df9279b3219906549d6ed4c4e6a7
Russia9 <a.kovalev9@hotmail.com>
2019-05-17 15:30:51 +0300

    Move to 0.3.3

--- src/main/java/io/github/russia9/magichelper/MagicHelper.java
+++ src/main/java/io/github/russia9/magichelper/MagicHelper.java
@@ -13,10 +13,10 @@ public class MagicHelper {
     private static final Logger logger = LogManager.getLogger(MagicHelper.class.getName());
 
     public static void main(String[] args) throws NativeHookException {
-        logger.info("MagicHelper v0.3.2 by Russia9");
+        logger.info("MagicHelper v0.3.3 by Russia9");
         logger.info("For more information: https://github.com/Russia9/MagicHelper");
 
-        logger.warn("MagicHelper v0.3.2 doesn't have full functional");
+        logger.warn("MagicHelper v0.3.3 doesn't have full functional");
 
         // Disabling standard JNativeHook logging
         java.util.logging.Logger logger = java.util.logging.Logger.getLogger(GlobalScreen.class.getPackage().getName());
