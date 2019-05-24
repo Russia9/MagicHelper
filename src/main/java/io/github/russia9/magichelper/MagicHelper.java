6c27435108389703c220af641d1e8a1899228dcb
Russia9 <a.kovalev9@hotmail.com>
2019-05-24 18:22:56 +0300

    Configurable debug output

--- src/main/java/io/github/russia9/magichelper/MagicHelper.java
+++ src/main/java/io/github/russia9/magichelper/MagicHelper.java
@@ -1,8 +1,10 @@
 package io.github.russia9.magichelper;
 
 import io.github.russia9.magichelper.lib.Helper;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.jnativehook.NativeHookException;
 
 
@@ -13,10 +15,13 @@ public class MagicHelper {
     private static final Logger logger = LogManager.getLogger(MagicHelper.class.getName());
 
     public static void main(String[] args) throws NativeHookException {
-        if (args.length > 0) {
-            logger.trace(args[0]);
+        for (String arg : args) {
+            if (arg.equals("Debug=true")) {
+                Configurator.setRootLevel(Level.DEBUG);
+            } else if (arg.equals("Trace=true")) {
+                Configurator.setRootLevel(Level.TRACE);
+            }
         }
-
         logger.info("MagicHelper v0.4 by Russia9");
         logger.info("For more information: https://github.com/Russia9/MagicHelper");
 
