e8037f17bde0a40a963df05906915c979b928b36
Russia9 <a.kovalev9@hotmail.com>
2019-05-24 15:39:41 +0300

    echo args

--- src/main/java/io/github/russia9/magichelper/MagicHelper.java
+++ src/main/java/io/github/russia9/magichelper/MagicHelper.java
@@ -13,6 +13,10 @@ public class MagicHelper {
     private static final Logger logger = LogManager.getLogger(MagicHelper.class.getName());
 
     public static void main(String[] args) throws NativeHookException {
+        if (args.length > 0) {
+            logger.trace(args[0]);
+        }
+
         logger.info("MagicHelper v0.3.3 by Russia9");
         logger.info("For more information: https://github.com/Russia9/MagicHelper");
         logger.warn("MagicHelper v0.3.3 doesn't have full functional");
